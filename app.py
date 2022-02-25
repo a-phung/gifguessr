9f07cf81e4740d9166868dab4f52faf3f8f81c76
a-phung <andyphunghb@gmail.com>
2022-02-25 01:05:53 -0800

    Built more of the frontend and backend of the website.

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, url_for
-import pika, os
+from flask import Flask, render_template
+from random import shuffle  # TODO: Remove and replace with teammate's microservice
+import requests
+
 
 app = Flask(__name__)
 
@@ -12,51 +14,29 @@ def home():
 
 @app.route("/play")
 def play():
-    # - Producer - Send a message to RabbitMQ
-    url = "amqps://tjybwijb:ljZM5GMaLrZtrc3K9OS0WGN6gWL3pmtB@jaguar.rmq.cloudamqp.com/tjybwijb"
-    params = pika.URLParameters(url)
-    connection = pika.BlockingConnection(params)
-    # Send message to fetch image from image microservice
-    channel = connection.channel()  # Start a channel
+    # TODO: Add image citations
+    # Make API call to image microservice
+    response = requests.get("https://image-microservice-us.herokuapp.com/gifguessr")
+    # Get back json object as a response
+    image_json = response.json()
+    # Json object format is {"image": "image url", "words": ["word1", "word2", "word3"]}
+    image_url = image_json["image"]
+    word1 = image_json["words"][0]
+    word2 = image_json["words"][1]
+    word3 = image_json["words"][2]
+    # Get hints for words
+    hint1 = shuffle_word(word1)
+    hint2 = shuffle_word(word2)
+    hint3 = shuffle_word(word3)
+    return render_template("play.html", image_url=image_url, word1=word1, word2=word2, word3=word3,
+                           hint1=hint1, hint2=hint2, hint3=hint3)
 
-    channel.exchange_declare("test_exchange")  # Declare exchange
-    channel.queue_declare(queue="test_queue")  # Declare queue
-    channel.queue_bind("test_queue", "test_exchange", "tests")  # Create binding between queue and exchange
 
-    # Produce message
-    channel.basic_publish(
-        body="Fetching new image...",
-        exchange="test_exchange",
-        routing_key="tests"
-    )
-    print("Message sent.")
-    channel.close()  # Close channel
-
-    # - Consumer - Open new channel to listen for image message to consume
-    channel = connection.channel()  # Start a channel
-    channel.queue_declare(queue="image_queue")  # Declare queue
-
-    # Initialize array to hold message body
-    image_array = []
-
-    # Get the message from RabbitMQ and display image
-    def callback(ch, method, properties, body):
-        print(" [x] Received " + body.decode())
-        image_array.append(body.decode())
-        channel.close()
-
-    channel.basic_consume(
-        "image_queue",
-        callback,
-        auto_ack=True
-    )
-
-    print(" [*] Waiting for messages: ")
-    channel.start_consuming()
-    connection.close()  # Close the connection
-
-    image_url = image_array[0]
-    return render_template("play.html", image_url=image_url)
+def shuffle_word(word):
+    """TODO: Replace with teammate's microservice."""
+    word_list = list(word)
+    shuffle(word_list)
+    return ''.join(word_list)
 
 
 if __name__ == "__main__":
